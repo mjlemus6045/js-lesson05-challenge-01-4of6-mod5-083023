00f0c38354191ad516ab733ca1edd47ffe196d67
mjlemus6045 <83889527+mjlemus6045@users.noreply.github.com>
2023-08-30 16:24:54 -0400

    updated script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var button = document.querySelector(".show-cat");
+var cat = document.querySelector(".cat");
+//console.log(button);
+//console.log(button);
+button.addEventListener("click", function () {
+    cat.classList.add("show");
+});
