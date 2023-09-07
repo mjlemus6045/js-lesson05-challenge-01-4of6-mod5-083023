8d6e6bc17a315dc346f15eb11e46d33746223977
mjlemus6045 <83889527+mjlemus6045@users.noreply.github.com>
2023-09-07 19:04:22 -0400

    Updated script.js with conditional statement

--- js/script.js
+++ js/script.js
@@ -3,5 +3,13 @@ var cat = document.querySelector(".cat");
 //console.log(button);
 //console.log(button);
 button.addEventListener("click", function () {
+  if (cat.classList.contains("show")) {
+    cat.classList.remove("show");
+    button.innerText = "Wait, come back!";
+    button.classList.add("disappear");
+   }  else {
     cat.classList.add("show");
+    button.innerText = "Shoo, cat!";
+    button.classList.remove("disappear");
+   }
 });
